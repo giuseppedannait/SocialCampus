259c330237ee794f48a71eea245f47aa99ba41ac
Giuseppe D'Anna <gd@giuseppedanna.com>
2018-05-30 13:56:00 +0200

    Auth Functionality

---
+++ resources/views/partials/user-menu.blade.php
@@ -0,0 +1,12 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">logged as {{ auth()->user()->email }} <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+        <li><a href="{{ action('HomeController@index') }}">Dashboard</a></li>
+        <li role="separator" class="divider"></li>
+        <li> <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+    </ul>
+</li>
